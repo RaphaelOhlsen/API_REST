d1aaf59b4d6d5a13ff2ec746d70d0d3eb77b637c
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-01-04 10:46:38 -0300

    Configurando e testando o Sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Teste',
+      sobrenome: 'Aparecida',
+      email: 'raphael.ohlsen@gmail.com',
+      idade: 50,
+      peso: 120,
+      altura: 1.72,
     });
+    res.json(novoAluno);
   }
 }
 
